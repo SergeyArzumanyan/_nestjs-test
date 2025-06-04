97647e7bbf96a143c02f2662b45a2225fa8d3720
Sergey Arzumanyan <sergo.arzumanyan000@gmail.com>
2025-06-04 23:58:50 +0400

    qwqwqwqw

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
 
-  app.enableCors();
-
   console.log(`Running on locahost:${PORT}`);
   await app.listen(PORT);
 }
