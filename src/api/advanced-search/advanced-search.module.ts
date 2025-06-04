69feb75f96d2f376112a6617d067cfd1d8905c87
Sergey Arzumanyan <sergo.arzumanyan000@gmail.com>
2025-06-05 00:03:37 +0400

    alla u abgar

--- src/api/advanced-search/advanced-search.module.ts
+++ src/api/advanced-search/advanced-search.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdvancedSearchService } from './advanced-search.service';
 import { AdvancedSearchController } from './advanced-search.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [AdvancedSearchController],
   providers: [AdvancedSearchService],
 })
